cbdd8d434d0474aad9435f7b2096080c878aa546
John Pontin <47417201+John-Pontin@users.noreply.github.com>
2024-06-26 13:18:13 +0100

    Something something something

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,5 +1,14 @@
+#' Title
+#'
+#' @param animal Something
+#' @param sound Something
+#'
+#' @return Something
+#' @export
+#'
+#' @examples Something
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
-  stopifnot(is.character(sound) & length(sound) == 1)
+  stopifdenot(is.character(sound) & length(sound) == 1)
   paste0("The ", animal, " goes ", sound, "!!")
 }
